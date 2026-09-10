ba92fddd151caf6e8151e5066a94078bd2d897ae
mmuhsincelebi-cyber <m.muhsincelebi@gmail.com>
2026-09-10 18:57:11 +0300

    Implement initial game menu and random number

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 int main()
 {
-    printf("Hello world!\n");
+    // number hunter project
+    int choice;
+    printf("=============================\n");
+    printf("   WELCOME TO NUMBER HUNTER   \n");
+    printf("=============================\n\n");
+    printf("1- Start game\n\n");
+    printf("2- Exit\n\n\n");
+    printf("Choose: ");
+    scanf("%d",&choice);
+    printf("\n");
+    if (choice == 1){
+        printf("Game starting...\n");
+        srand(time(NULL));
+        int secretnumber = rand() %100 + 1;
+        printf("The computer has chosen a number: %d\n",secretnumber);
+    }
+    else if (choice == 2){
+        printf("Goodbye!");
+    }
+    else {
+        printf("Invalid choice!!");
+    }
     return 0;
 }
